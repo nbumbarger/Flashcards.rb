09b96b1845088df7440026673a244ec2ae5df31c
Nick Bumbarger <nbumbarger@gmail.com>
2015-06-18 17:46:39 -0400

    added font precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( normalize.css )
 Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
